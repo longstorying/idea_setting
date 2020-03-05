88b0d4e25bc829446a8b374fd110a7ea0c435164
10652 <10652@10.0.75.1>
2020-03-05 09:55:04 +0800

    IU-2019.3.3 <10652@A00442 Create WWDC16-mod.icls, markdown.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Default copy.xml, yiiguxing.translation.xml, Default.xml, find.xml, keymap.xml, KeyPromoterXSettings.xml, my-keys.xml, mavenVersion.xml, Tomorrow Night.icls, javarebel.xml, GapStyle Sublime.icls, Default.xml, docker.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, Vibrant Unknown (Darcula).icls, ide.general.xml, git.xml, CodeGlance.xml, rainbow_brackets.xml, lombok-plugin.xml, ui.lnf.xml, filetypes.xml, remote-servers.xml, colors.scheme.xml, notifications.xml, AiXCoderConfig_local.xml, VisualVMLauncher.xml, databaseDrivers.xml, templates.xml, jprofiler.xml, Neon Night Update 1.icls, studioFlags.xml, laf.xml, parameter.hints.xml, Class.java, jdk.table.xml, path.macros.xml, intentionSettings.xml, wsl.distributions.xml, proxy.settings.xml, toStringTemplates.xml, mybatis.xml, usageView.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @author ${USER}
+* @date ${DATE} ${TIME}
+*/
+public class ${NAME} {
+}
